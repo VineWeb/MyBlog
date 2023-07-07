27ea9905fd6503883fae65ec93cb53144d0c7666
Vine <chenjinbode@yeah.net>
2023-07-07 19:54:34 +0800

    更新iconfont

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -12,27 +12,27 @@ import {
 } from "./chunk-V634PGSD.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default3 } from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default4 } from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default5 } from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default6 } from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default7 } from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default8 } from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default9 } from "C:/Users/bian/Desktop/web-imooc/vine-press/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "C:/Users/bian/Desktop/web-imooc/MyBlog/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/@vueuse/shared/index.mjs
 function tryOnScopeDispose(fn) {
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  head: [
+    ['link', { rel: 'icon', href: './favicon.ico' }] // 如果你使用的是自定义文件名，请在此处更新文件名
+  ],
   base: '/MyBlog/',
   theme: {
     outlineTitle: '在此页'
@@ -7,9 +10,13 @@ module.exports = {
   host: '0.0.0.0',
   port: '3002',
   title: '欢迎使用VineCode的文档',
+  titleTemplate: '欢迎使用VineCode的文档',
   description: '前端文档，不止前端文档。',
   lastUpdated: true,
   themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/ITdian/MyBlog' },
+    ],
     outlineTitle: '本页面',
     docFooter: {
       prev: '上一页',
@@ -48,6 +55,10 @@ module.exports = {
         ],
         }
       ]
+    },
+    footer: {
+      message: '念念不忘，必有回响',
+      copyright: 'Email: itdian.666@163.com'
     }
   },
 }
